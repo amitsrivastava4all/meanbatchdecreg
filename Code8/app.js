ecea0859f01752899625208b930672980580e76b
amitsrivastava4all <amitsrivastava4all@gmail.com>
2019-02-18 14:22:41 +0530

    Code

---
+++ Code8/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app =  express();
+const bodyParser = require("body-parser");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/',require("./routes/user"));
+app.use('/products',require("./routes/product"));
+app.use((request, response,next)=>{
+    response.send('U Type Something Wrong...');
+})
+
+app.listen(process.env.PORT || 1234,(err)=>{
+    if(err){
+        throw err;
+    }
+    else{
+        console.log("Server Started At 1234");
+    }
+})
